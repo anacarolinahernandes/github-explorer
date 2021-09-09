50bbfdc0d674b07a536c3e0dd702dfacb2696099
Ana Carolina Hernandes <anacbhernandes@gmail.com>
2021-09-09 16:09:36 -0300

    ajustando configs e adicionando pacote de fast refresh

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 const isDev = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   mode: isDev ? 'development' : 'production',
-  devtool: isDev ? 'eval-source-map' : 'source-map', 
+  devtool: isDev ? 'eval-source-map' : 'source-map',
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -17,18 +18,29 @@ module.exports = {
   devServer: {
     static: path.resolve(__dirname, 'public'),
     port: 3000,
+    hot: true,
   },
   plugins: [
+    isDev && new ReactRefreshWebpackPlugin({
+      overlay: false,
+    }),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html'),
     })
-  ],
+  ].filter(Boolean),
   module: {
     rules: [
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
-        use: 'babel-loader',
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              isDev && require.resolve('react-refresh/babel')
+            ].filter(Boolean),
+          },
+        },
       },
       {
         test: /\.scss$/,
